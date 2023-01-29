92fb897caf5f75e9d188abce3e22251f6204e01b
Andres Camilo Rios <acamilorm4556@gmail.com>
2023-01-29 14:54:19 -0500

    https added

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ export default function App() {
   const apiKey = '12adc7541d84109f45a2c7ac18d5212a'
 
   function onSearch(ciudad) {
-    fetch(`http://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&appid=${apiKey}&units=metric`)
       .then(r => r.json())
       .then((recurso) => {
         if(recurso.main !== undefined){
